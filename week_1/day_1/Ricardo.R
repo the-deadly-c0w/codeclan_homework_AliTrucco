43b4597e8d0b34757dce75ca7ddb19626d63e51a
the-deadly-c0w <masdofswords@gmail.com>
2023-05-22 18:07:23 +0100

    Ricardo's sorry end
    What a goat, long may he rest in our non-vegan dairy substitute memories,
    
    Amen

--- week_1/day_1/Ricardo.R
+++ week_1/day_1/Ricardo.R
@@ -1,3 +1,9 @@
 # There once was a man called Ricardo
 
 # Who's pockets were brimming with lard, oh
+
+# When he stepped in the oven
+
+# Though the heat he was lovin'
+
+# His lard had ceased to be hard, oh...... no
